edb33ae1a2975d2b65cc13a1ef3fe25999071c5a
liamocuz <liamo@me.com>
2021-08-05 16:43:06 -0400

    Moved definitions

--- include/encode.h
+++ include/encode.h
@@ -1,9 +1,5 @@
 #include "tree.h"
 
-// Constants
-#define ENCODE_SUCCESS 1
-#define ENCODE_FAILURE -1
-
 // Function definitions
 int countChars(char* input, int* asciiCount, long int* uniqueChars, long int* totalCharsUncompressed);
 int compress(char* input, char* output);
